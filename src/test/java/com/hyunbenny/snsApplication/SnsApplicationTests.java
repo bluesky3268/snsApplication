139ace67801decd7c630f0733ba52c1405044a2f
hbcho <bluesky3268@gmail.com>
2023-04-14 13:10:10 +0900

    initial commit

---
+++ src/test/java/com/hyunbenny/snsApplication/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunbenny.snsApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
