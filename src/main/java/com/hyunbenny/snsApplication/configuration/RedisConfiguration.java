6d6a370b559918b7ea60ad2f2253c583d0ec8e17
hbcho <bluesky3268@gmail.com>
2023-04-26 17:30:51 +0900

    #19 [enhancement] - redis 설정
    - reids 사용을 위한 redis 설정 정보 추가

---
+++ src/main/java/com/hyunbenny/snsApplication/configuration/RedisConfiguration.java
@@ -0,0 +1,45 @@
+package com.hyunbenny.snsApplication.configuration;
+
+import com.hyunbenny.snsApplication.model.User;
+import io.lettuce.core.RedisURI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableRedisRepositories
+@RequiredArgsConstructor
+public class RedisConfiguration {
+
+    /**
+     * 캐싱할 때, 데이터의 변경이 너무 잦은 데이터는 캐싱의 의미가 없다.
+     * 따라서 변경이 많지 않고, 접근을 자주 하는(자주 사용하는) 데이터를 캐싱하는 것이 좋다.
+     */
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisURI redisURI = RedisURI.create(redisProperties.getUrl());
+        org.springframework.data.redis.connection.RedisConfiguration configuration = LettuceConnectionFactory.createRedisConfiguration(redisURI);
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(configuration);
+        factory.afterPropertiesSet();
+        return factory;
+    }
+    @Bean
+    public RedisTemplate<String, User> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<User>(User.class));
+        return redisTemplate;
+    }
+
+}
